7bba1582587245a1b6e371377442d3f1aa78dc15
Yvi-compt <yvesplacide@gmail.com>
2025-02-28 17:49:41 -0500

    Update variables.tf

--- yvi-terraform/variables.tf
+++ yvi-terraform/variables.tf
@@ -1,1 +1,12 @@
 
+variable "azure_region" {
+  description = "La région Azure où les ressources seront déployées"
+  type        = string
+  default     = "East US"
+}
+
+variable "resource_group_name" {
+  description = "Nom du groupe de ressources"
+  type        = string
+  default     = "socialmedia-rg"
+}
